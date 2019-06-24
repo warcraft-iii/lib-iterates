9389b3b5a8d39e3130874bbc172d6d7048880935
Dencer <tdaddon@163.com>
2019-06-25 00:26:35 +0800

    feat: Add enumUnitsOfPlayer iterateUnitsOfPlayer

---
+++ init.lua
@@ -0,0 +1,6 @@
+-- init.lua
+-- @Author : Dencer (tdaddon@163.com)
+-- @Link   : https://dengsir.github.io
+-- @Date   : 6/24/2019, 10:01:01 PM
+
+Iterate = require('lib.iterates.iterate')
---
+++ iterate.lua
@@ -0,0 +1,35 @@
+-- iterate.lua
+-- @Author : Dencer (tdaddon@163.com)
+-- @Link   : https://dengsir.github.io
+-- @Date   : 6/24/2019, 10:01:13 PM
+
+local Iterate = {}
+
+local Group = require('lib.oop.group')
+
+---enumUnitsOfPlayer
+---@param player Player
+---@param callback UnitCallback
+function Iterate:enumUnitsOfPlayer(player, callback)
+    local group = Group:create()
+    group:enumUnitsOfPlayer(player)
+    group:forEach(callback)
+end
+
+---iterateUnitsOfPlayer
+---@param player Player
+---@return fun(): Unit
+function Iterate:iterateUnitsOfPlayer(player)
+    local group = Group:create()
+    group:enumUnitsOfPlayer(player)
+
+    return coroutine.wrap(function()
+        local unit = group:firstOf()
+        if unit then
+            coroutine.yield(unit)
+            group:removeUnit(unit)
+        end
+    end)
+end
+
+return Iterate
